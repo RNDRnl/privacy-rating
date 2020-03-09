56957f62ce559223452dcc1a481b85ed9cb7eaea
BoydRotgans <boyd@rndr.studio>
2020-03-09 12:39:31 +0100

    configure fonts

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,14 @@ module.exports = {
           },
         'sass-loader',
         ]
+      },
+      {
+        test: /\.(woff2?|ttf|otf|eot|svg)$/,
+        exclude: /node_modules/,
+        loader: 'file-loader',
+        options: {
+            name: '[path][name].[ext]'
+        }
       }
     ]
   },
