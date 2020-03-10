55f67e738aa9b9400418ca2b66bc6897aaad1735
jasperkamphuis <jasper@rndr.studio>
2020-03-10 17:50:22 +0100

    Updated Home page + added folder logos

--- src/js/componentsForm/Form.js
+++ src/js/componentsForm/Form.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, Container, Row, Col, Form } from 'react-bootstrap';
+import React, from 'react';
+import { Button, Container, Row, Form, Col } from 'react-bootstrap';
 
 const initialFormState = {
 }
@@ -10,8 +10,15 @@ const FormView = () => {
             <Row>
                 <Button>hi i'm a form!</Button>
             </Row>
+
+            <p>
+                When multiple awnsers apply:
+                Choose the default use case.
+                Consider the wors-case senario.
+            </p>
             <Row>
                 <Form>
+
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" />
@@ -27,11 +34,40 @@ const FormView = () => {
                     <Form.Group controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+
+                    <fieldset>
+                        <Form.Group as={Row}>
+                            <Form.Label as="legend" column sm={2}>
+                                Radios
+                            </Form.Label>
+                            <Col sm={10}>
+                                <Form.Check
+                                    type="radio"
+                                    label="first radio"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios1"
+                                />
+                                <Form.Check
+                                    type="radio"
+                                    label="second radio"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios2"
+                                />
+                                <Form.Check
+                                    type="radio"
+                                    label="third radio"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios3"
+                                />
+                            </Col>
+                        </Form.Group>
+                    </fieldset>
+                    <Button variant="primary" type="submit" href="1">
                         Submit
                     </Button>
                 </Form>
             </Row>
+
         </Container>
     );
 };
--- src/js/componentsHome/Home.js
+++ src/js/componentsHome/Home.js
@@ -1,32 +1,83 @@
 import React, { Component } from "react";
+import {Button, Container, Row, Jumbotron, Image, Col, Card, CardDeck} from "react-bootstrap";
 
-class Home extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            value: ""
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(event) {
-        const { value } = event.target;
-        this.setState(() => {
-            return {
-                value
-            };
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <div>HomePage</div>
-            </div>
-        );
-    }
+const initialFormState = {
 }
 
-export default Home;
+const FormView = () => {
+    return (
+        <Container>
+            <Row>
+                <Jumbotron>
+                    <h1>PRIVACY RATING</h1>
+
+                    <h2>About</h2>
+                    <p>
+                        Privacy Rating specializes in helping enterprises comply
+                        with client-side privacy and data leakage
+                        regulations by protecting the customers' private data.
+                        The company's qprivacy is a solution that allows businesses
+                        that operate within highly regulated environments, such as financial services institutions,
+                        to scrutinize and enforce their own data privacy policy and prevent the flow
+                        of unauthorized communication from customers' devices.
+                        Privacy Rating specializes in helping enterprises comply
+                        with client-side privacy and data leakage
+                        regulations by protecting the customers' private data.
+                        The company's qprivacy is a solution that allows businesses
+                        that operate within highly regulated environments, such as financial services institutions,
+                        to scrutinize and enforce their own data privacy policy and prevent the flow
+                        of unauthorized communication from customers' devices.
+                    </p>
+
+                    <h2>Create your own label</h2>
+
+                    <p>
+                        creating a label your self all you have to do is take a questionnaire about your website and the
+                        label will be generated for your website and made to implement easily in to your own website.
+                    </p>
+
+                    <p>
+                        <Button variant="success" href="form">Click here to create a label</Button>{' '}
+                    </p>
+
+                    <h4> Websites who already use the label</h4>
+
+                    <Row>
+                        <CardDeck>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Img variant="top" src="resources/logos/rijksoverheid.jpg" />
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Rijksoverheid</small>
+                                </Card.Footer>
+                            </Card>
+
+                            <Card>
+                                <Card.Body>
+                                    <Card.Img variant="top" src="resources/logos/eu.jpg" />
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">EU Parlement</small>
+                                </Card.Footer>
+                            </Card>
+
+                            <Card>
+                                <Card.Body>
+                                    <Card.Img variant="top" src="resources/logos/bol.jpg" />
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Bol.com</small>
+                                </Card.Footer>
+                            </Card>
+
+                        </CardDeck>
+                    </Row>
+
+                </Jumbotron>
+            </Row>
+        </Container>
+    );
+};
+
+export default FormView;
