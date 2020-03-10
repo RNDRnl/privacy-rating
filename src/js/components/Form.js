fe331940f5d5d240d117d0499b8a6ef01588aa03
BoydRotgans <boyd@rndr.studio>
2020-03-10 13:54:55 +0100

    bootstrap

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { Button, Container, Row, Col, Form } from 'react-bootstrap';
 
 const initialFormState = {
 }
 
-const Form = () => {
+const FormView = () => {
     return (
-        <div>hi i'm a form!</div>
+        <Container>
+            <Row>
+                <Button>hi i'm a form!</Button>
+            </Row>
+            <Row>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Row>
+        </Container>
     );
 };
 
-export default Form;
+export default FormView;
