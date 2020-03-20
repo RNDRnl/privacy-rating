6f1d8457b288dd6a2cf148d4f88d557d5e1f6e08
jasperkamphuis <jasper@rndr.studio>
2020-03-20 14:41:00 +0100

    responsive header

--- src/js/Header.js
+++ src/js/Header.js
@@ -8,19 +8,16 @@ const Header= () => {
     return (
         <div  className={styles.holder} >
 
-            <div className={styles.circlebox}>
-
-                <div className={styles.cirleG}>G</div>
-                <div className={styles.cirleF}>F</div>
-                <div className={styles.cirleE}>E</div>
-                <div className={styles.cirleD}>D</div>
-                <div className={styles.cirleC}>C</div>
-                <div className={styles.cirleB}>B</div>
-                <div className={styles.cirleA}>A</div>
-
-            </div>
-
+            <div className={styles.flexbox}>
                 <div className={styles.privacyRatingFont}>PRIVACY<br/>RATING</div>
+                <div className={styles.cirleA}>A</div>
+                <div className={styles.cirleB}>B</div>
+                <div className={styles.cirleC}>C</div>
+                <div className={styles.cirleD}>D</div>
+                <div className={styles.cirleE}>E</div>
+                <div className={styles.cirleF}>F</div>
+                <div className={styles.cirleG}>G</div>
+            </div>
 
         </div>
     );
