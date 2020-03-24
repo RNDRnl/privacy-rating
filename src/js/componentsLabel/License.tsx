34968180322699bbb4173db88e7aea5f137554f5
jasperkamphuis <jasper@rndr.studio>
2020-03-24 10:47:53 +0100

    CC license

---
+++ src/js/componentsLabel/License.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import {Image, OverlayTrigger, Tooltip} from "react-bootstrap";
+const styles = require("./License.scss");
+
+function renderTooltip(props) {
+    return <Tooltip className={styles.license} {...props}>
+        <div className={styles.license}>
+            <Image className={styles.tooltip} src="/resources/icons/by.svg"/>
+            <Image className={styles.tooltip} src="/resources/icons/nc-eu.svg"/>
+            <Image className={styles.tooltip} src="/resources/icons/nd.svg"/>
+            <div className={styles.licenseText}>3.0</div>
+        </div>
+    </Tooltip>;
+}
+
+const CreativeCommons = () => (
+    <OverlayTrigger
+        placement="right"
+        delay={{ show: 250, hide: 400 }}
+        overlay={renderTooltip}
+    >
+        <div className={styles.explain}>
+            <a href={"https://creativecommons.org/licenses/by-nc-nd/3.0/ "} target="_blank" title="Creative Commons"/>
+        </div>
+    </OverlayTrigger>
+);
+
+class License extends React.Component{
+    render() {
+        return (
+            <div className={styles.license}>
+                <CreativeCommons/>
+            </div>
+        );
+    }
+}
+
+export default License;
