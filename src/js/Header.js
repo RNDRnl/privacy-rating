0687916bdc3499b994bb2d7fa754ce594587972a
boydrotgans <boyd@lustlab.net>
2020-04-15 15:49:07 +0200

    homepage update

--- src/js/Header.js
+++ src/js/Header.js
@@ -3,19 +3,14 @@ import styles from './Header.scss';
 import classnames from "classnames";
 import { Link } from "react-router-dom";
 
-const initialFormState = {
-}
-
 const Header= () => {
     return (
-        <div>
         <div  className={styles.holder}>
             <Link to="/" className={styles.flexboxTitle}>   
                 <div className={classnames(styles.element, styles.privacyRatingFont)}>PRIVACY<br/>RATING</div>
             </Link>
             
             <div className={styles.flexbox}>
-               
                 <div className={classnames(styles.element)}>
                     <div className={classnames(styles.label, styles.circleA)}>A</div>
                 </div>
@@ -38,12 +33,6 @@ const Header= () => {
                     <div className={classnames(styles.label, styles.circleG)}>G</div>
                 </div>
             </div>
-        </div>
-            {/* <div  className={styles.holderSmall} >
-                <div className={styles.footerText}>
-                    Project by the <a target="_blank" href="https://www.utwente.nl/">University of Twente</a> supported by <a target="_blank" href="https://www.nwo.nl/">NWO</a> in collaboration with <a target="_blank" href="https://www.tno.nl/nl/">TNO</a>, <a target="_blank" href="https://wodc.nl/">WODC</a>, <a target="_blank" href="https://www.centric.eu/">Centric B.V.</a>
-                </div>
-            </div> */}
         </div>
     );
 };
