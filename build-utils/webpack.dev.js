a8982171796a3ca2ab0d42ed7b77bb57a1b23ae4
BoydRotgans <boyd@rndr.studio>
2020-04-16 23:03:54 +0200

    deployment

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -38,7 +38,7 @@ const config = {
         { loader: "style-loader" },  // to inject the result into the DOM as a style block
         { loader: "css-modules-typescript-loader"},  // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
         { loader: "css-loader", options: { modules: true } },  // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
-        { loader: "sass-loader", options: { prependData: '$env: ' + process.env.NODE_ENV + ';'} },  // to convert SASS to CSS
+        { loader: "sass-loader", options: { prependData: '$env: \'/\';'} },  // to convert SASS to CSS
         // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
     ] }, 
       {
