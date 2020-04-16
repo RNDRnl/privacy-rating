cd4f08b0ecbc03d792781d8912f6cdbc8d069d9b
boydrotgans <boyd@lustlab.net>
2020-04-16 10:22:05 +0200

    home fix buttons

--- src/js/componentsHome/Home.js
+++ src/js/componentsHome/Home.js
@@ -1,68 +1,15 @@
 import React, { Component } from "react";
 import styles from './Home.scss';
-import {Button, Container, Row, Modal} from "react-bootstrap";
+import {Button, Container, Row, Modal, Jumbotron, Image, Col, Card, CardDeck} from "react-bootstrap";
 import Header from "../Header";
 import Footer from "../Footer";
 import { Link } from "react-router-dom";
 import ImageGalleryView from "./ImageGalleryView";
 
-const initialFormState = {
-}
-
-function MyVerticallyCenteredModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Privacy rating demo
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body style={{padding:"50px"}}>
-        <iframe style={{display:'block', marginLeft:'auto', marginRight:'auto'}} src={"/ACABCBABCAAA"}
-                                                top="0"
-                                                left="0"
-                                                width="430pt"
-                                                height="430pt"
-                                                border="0"
-                                                overflow="hidden"
-                                                margin="0"
-                                                padding="0"
-                                                border-radius="8pt"
-                                                frameBorder="0"
-                                                gesture="media"
-                                                allow="encrypted-media"
-                                                allowFullScreen/>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide} variant="dark">Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-
 class Home extends Component {
-
 
     constructor() {
         super();
-
-        this.state = {
-          show: false
-        }
-
-        this.setModalShow = this.setModalShow.bind(this);
-
-    }
-
-    setModalShow(_showState) {
-        this.setState({
-            show:_showState
-        })
     }
 
     render() {
@@ -70,24 +17,18 @@ class Home extends Component {
         <div  className={styles.holder} >
             <Container>
                 <Header/>
-
-                <MyVerticallyCenteredModal
-                    show={this.state.show}
-                    onHide={() => this.setModalShow(false)}
-                 />
-
                 <Row className={styles.backgroundSlider}>
                     <div className={styles.label}>
                         <ImageGalleryView />
                         <br />    
                     </div>
-
                     <div className={styles.text}>
                         <div>
                             <p>
                                 As online services handle increasing amounts of
                                 data, it becomes difficult to inform users about
-                                how their information is handled.</p>
+                                how their information is handled.
+                            </p>
                             <p>
                                 Nevertheless, more and more regulations and
                                 standards require transparency when handling
@@ -97,7 +38,8 @@ class Home extends Component {
                         <div >
                             <p>
                                 Privacy policies describe how services handle
-                                user data, but most users never read them.</p>
+                                user data, but most users never read them.
+                            </p>
                             <p>
                                 PrivacyRating.info creates an intuitive
                                 visualization of your privacy policy that users
@@ -109,23 +51,17 @@ class Home extends Component {
                     </div>
                     <div className={styles.actions}>
                         <div>
-                        <Link className={styles.button} to="/" onClick={() => this.setModalShow(true)} >
-                           Demo
-                        </Link>
+                            <Link className={styles.button} to="/about">
+                                Learn more
+                            </Link>
                         </div>
                         <div>
-                        <Link className={styles.button} to="/about">
-                           About
-                        </Link>
-                        </div>
-                        <div>
-                        <Link className={styles.button} to="/form">
-                            Generate your own label
-                        </Link>
+                            <Link className={styles.button} to="/form">
+                                Generate your own label
+                            </Link>
                         </div>
                     </div>
                 </Row>
-
             <br />
             <Footer/>
             </Container>
